13f953ecd4de02c3d152581d5d612befbe19b632
nkowne63rt <neutron63zf@gmail.com>
2022-10-12 13:20:49 +0900

    init: sample workload

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,34 @@ static APP_BEARER_TOKEN: Lazy<String> = Lazy::new(|| {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use time::macros::datetime;
+    use twitter_v2::authorization::BearerToken;
+    use twitter_v2::query::TweetField;
+    use twitter_v2::TwitterApi;
 
     #[test]
     #[ignore]
     fn it_works() {
         println!("APP_BEARER_TOKEN = {}", APP_BEARER_TOKEN.as_str());
+    }
+
+    #[tokio::test]
+    async fn sample_twitter_v2() -> Result<(), twitter_v2::Error> {
+        let auth = BearerToken::new(APP_BEARER_TOKEN.as_str());
+        let tweet = TwitterApi::new(auth)
+            .get_tweet(1261326399320715264)
+            .tweet_fields([TweetField::AuthorId, TweetField::CreatedAt])
+            .send()
+            .await?
+            .into_data()
+            .expect("this tweet should exist");
+        assert_eq!(tweet.id, 1261326399320715264);
+        assert_eq!(tweet.author_id.unwrap(), 2244994945);
+        assert_eq!(
+            tweet.created_at.unwrap(),
+            datetime!(2020-05-15 16:03:42 UTC)
+        );
+
+        Ok(())
     }
 }
