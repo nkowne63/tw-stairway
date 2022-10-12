241e8dfeefc32c735a9451a0e81956453c2cbf61
nkowne63rt <neutron63zf@gmail.com>
2022-10-12 17:13:32 +0900

    add: funcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use dotenvy::dotenv;
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
+use twitter_v2::{authorization::BearerToken, id::NumericId, ApiPayload, Error, User};
 
 static ENV: Lazy<HashMap<String, String>> = Lazy::new(|| {
     dotenv().ok();
@@ -12,6 +13,77 @@ static APP_BEARER_TOKEN: Lazy<String> = Lazy::new(|| {
         .expect("APP_BEARER_TOKEN is not set")
         .to_string()
 });
+
+static CLIENT: Lazy<twitter_v2::TwitterApi<BearerToken>> =
+    Lazy::new(|| twitter_v2::TwitterApi::new(BearerToken::new(APP_BEARER_TOKEN.as_str())));
+
+enum Relation {
+    Following,
+    Followers,
+}
+
+async fn get_related_ids(id: NumericId, rel: Relation) -> Result<Vec<NumericId>, Error> {
+    let mut ids = Vec::new();
+    let mut next_token: Option<String> = None;
+    loop {
+        let mut request = match rel {
+            Relation::Following => CLIENT.get_user_following(id),
+            Relation::Followers => CLIENT.get_user_followers(id),
+        };
+        let mut request = request.max_results(1000);
+        if let Some(token) = next_token {
+            request = request.pagination_token(token.as_str());
+        }
+        let ApiPayload {
+            data,
+            meta,
+            includes: _,
+            errors: _,
+        } = request.send().await?.into_payload();
+        if let Some(users) = data {
+            if users.is_empty() {
+                break;
+            }
+            let current_ids = users
+                .into_iter()
+                .map(|user| user.id)
+                .collect::<Vec<NumericId>>();
+            ids.extend(current_ids);
+        } else {
+            break;
+        }
+        if let Some(meta) = meta {
+            if meta.next_token.is_none() {
+                break;
+            }
+            let temp_next_token = meta.next_token.expect("there exists next_token");
+            next_token = Some(temp_next_token);
+        } else {
+            break;
+        }
+    }
+    Ok(ids)
+}
+
+async fn get_self_user() -> Result<User, Error> {
+    let user = CLIENT
+        .get_user_by_username(ENV.get("SELF_USERNAME").unwrap().as_str())
+        .send()
+        .await?
+        .into_data()
+        .expect("this user should exist");
+    Ok(user)
+}
+
+async fn get_self_lv2() -> Result<Vec<NumericId>, Error> {
+    let self_user = get_self_user().await?;
+    let followings = get_related_ids(self_user.id, Relation::Following).await?;
+    for following in followings {
+        let following_lv2 = get_related_ids(following, Relation::Following).await?;
+        println!("count: {}", following_lv2.len());
+    }
+    todo!()
+}
 
 #[cfg(test)]
 mod tests {
@@ -44,6 +116,25 @@ mod tests {
             datetime!(2020-05-15 16:03:42 UTC)
         );
 
+        Ok(())
+    }
+
+    #[tokio::test]
+    #[ignore = "this test is printing something"]
+    async fn sample_get_follows_and_following() -> Result<(), twitter_v2::Error> {
+        let user = get_self_user().await?;
+        println!("user = {:#?}", user);
+        let followings = get_related_ids(user.id, Relation::Following).await?;
+        println!("followings = {:#?}", followings.len());
+        let followers = get_related_ids(user.id, Relation::Followers).await?;
+        println!("followings = {:#?}", followers.len());
+        Ok(())
+    }
+
+    #[tokio::test]
+    #[ignore = "this test is printing something"]
+    async fn get_lv2_followings() -> Result<(), twitter_v2::Error> {
+        let _ = get_self_lv2().await?;
         Ok(())
     }
 }
