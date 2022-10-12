b18d532dcdff561a8d8811458f9fd4727195df11
nkowne63rt <neutron63zf@gmail.com>
2022-10-13 01:16:58 +0900

    complete: basic suggester

--- src/suggest.rs
+++ src/suggest.rs
@@ -4,28 +4,46 @@ use twitter_v2::{Error, User};
 use crate::{
     env::get_self_user,
     relation::{get_related_ids, get_replying_users, Relation},
+    utils::get_user_by_id,
 };
 
 pub async fn get_suggested_users() -> Result<Vec<User>, Error> {
     let self_user = get_self_user().await?;
+    println!("self user: {:#?}", self_user);
     let self_followers = get_related_ids(self_user.id, Relation::Followers).await?;
-    let (first_chunk, _) = self_followers.split_at(2);
-    println!("first_chunk = {:#?}", first_chunk);
+    let (first_chunk, _) = self_followers.split_at(32);
+    println!("first_chunk.len() = {:#?}", first_chunk.len());
     let replying_users_distinct = join_all(
         first_chunk
             .iter()
             .map(|u| async move { get_replying_users(u.username.clone()).await }),
     )
     .await;
-    println!("replying_users_distinct = {:#?}", replying_users_distinct);
-    // let replying_users_distinct = replying_users_distinct
-    //     .iter()
-    //     .flatten()
-    //     .flat_map(|v| v.iter())
-    //     .filter(|nid| !self_followers.iter().map(|u| u.id).any(|x| x == **nid))
-    //     .collect::<Vec<_>>();
-    // println!("replying_users_distinct = {:#?}", replying_users_distinct);
-    Ok(vec![])
+    let mut replying_users_distinct = replying_users_distinct
+        .iter()
+        .flatten()
+        .flat_map(|v| v.iter())
+        .filter(|nid| {
+            !self_followers.iter().map(|u| u.id).any(|x| x == **nid) && nid != &&self_user.id
+        })
+        .collect::<Vec<_>>();
+    replying_users_distinct.dedup();
+    println!(
+        "replying_users_distinct.len = {:#?}",
+        replying_users_distinct.len()
+    );
+    let replying_users_distinct = join_all(
+        replying_users_distinct
+            .iter()
+            .map(|nid| async move { get_user_by_id(**nid).await }),
+    )
+    .await;
+    let replying_users_distinct = replying_users_distinct
+        .iter()
+        .flatten()
+        .cloned()
+        .collect::<Vec<_>>();
+    Ok(replying_users_distinct)
 }
 
 #[cfg(test)]
@@ -36,7 +54,9 @@ mod tests {
     #[ignore = "this test will print"]
     async fn sample_get_suggested_users() -> Result<(), twitter_v2::Error> {
         let suggested_users = get_suggested_users().await?;
-        println!("suggested_users = {:#?}", suggested_users);
+        for user in suggested_users {
+            println!("https://twitter.com/{}", user.username);
+        }
         Ok(())
     }
 }
