9c22ebc184ed3115612bf1e1cc947ede367342ce
nkowne63rt <neutron63zf@gmail.com>
2022-10-13 17:28:35 +0900

    fix: dedup

--- src/suggest.rs
+++ src/suggest.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 use futures::future::join_all;
 use twitter_v2::{Error, User};
 
@@ -11,7 +13,7 @@ pub async fn get_suggested_users() -> Result<Vec<User>, Error> {
     let self_user = get_self_user().await?;
     println!("self user: {:#?}", self_user);
     let self_followers = get_related_ids(self_user.id, Relation::Followers).await?;
-    let (first_chunk, _) = self_followers.split_at(32);
+    let (first_chunk, _) = self_followers.split_at(128);
     println!("first_chunk.len() = {:#?}", first_chunk.len());
     let replying_users_distinct = join_all(
         first_chunk
@@ -19,23 +21,19 @@ pub async fn get_suggested_users() -> Result<Vec<User>, Error> {
             .map(|u| async move { get_replying_users(u.username.clone()).await }),
     )
     .await;
-    let mut replying_users_distinct = replying_users_distinct
+    let replying_users_distinct = replying_users_distinct
         .iter()
         .flatten()
         .flat_map(|v| v.iter())
         .filter(|nid| {
             !self_followers.iter().map(|u| u.id).any(|x| x == **nid) && nid != &&self_user.id
         })
-        .collect::<Vec<_>>();
-    replying_users_distinct.dedup();
-    println!(
-        "replying_users_distinct.len = {:#?}",
-        replying_users_distinct.len()
-    );
+        .copied()
+        .collect::<BTreeSet<_>>();
     let replying_users_distinct = join_all(
         replying_users_distinct
             .iter()
-            .map(|nid| async move { get_user_by_id(**nid).await }),
+            .map(|nid| async move { get_user_by_id(*nid).await }),
     )
     .await;
     let replying_users_distinct = replying_users_distinct
@@ -43,6 +41,10 @@ pub async fn get_suggested_users() -> Result<Vec<User>, Error> {
         .flatten()
         .cloned()
         .collect::<Vec<_>>();
+    println!(
+        "replying_users_distinct.len() = {:#?}",
+        replying_users_distinct.len()
+    );
     Ok(replying_users_distinct)
 }
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,14 @@
-use twitter_v2::{id::NumericId, Error, User};
+use twitter_v2::{id::NumericId, query::UserField, Error, User};
 
 use crate::env::CLIENT;
 
 pub async fn get_user_by_id(id: NumericId) -> Result<User, Error> {
-    let user = CLIENT.get_user(id).send().await?.into_data().unwrap();
+    let user = CLIENT
+        .get_user(id)
+        .user_fields([UserField::Username])
+        .send()
+        .await?
+        .into_data()
+        .unwrap();
     Ok(user)
 }
